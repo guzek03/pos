62fda9150ad81556b8751b123dcbebc5a94896bd
Agus Dwi Krismono <dwiagus85@gmail.com>
2024-06-05 11:01:28 +0700

    update: modifications master items and add errors notif

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
-  before_action :set_param, only: %i[ new edit ]
+  before_action :set_param, only: %i[ new edit create update ]
 
   # GET /items or /items.json
   def index
-    @items = Item.all
+    @items = Item.includes(:uom)
   end
 
   # GET /items/1 or /items/1.json
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
+        format.html { redirect_to items_url, notice: "Item was successfully created." }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
+        format.html { redirect_to items_url, notice: "Item was successfully updated." }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :uom
+  validates :sku, presence: true
+  validates :sku, uniqueness: true
 end
